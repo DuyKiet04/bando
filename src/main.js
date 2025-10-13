4b1eae6589e539fbe5376dba63c26a53603ef2a4
DuyKiet04 <nguyenduykiet05072004@gmail.com>
2025-10-13 13:20:47 +0700

    fix lỗi hiển thị map

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
       // thư viện leaflet
       import L from "leaflet"; 
       import "leaflet/dist/leaflet.css";
-      
       //  tọa độ trung tâm (10.780221592300679, 106.68759855218681) 273 Điện Biên Phủ , Phường Xuân Hòa , Thành Phố Hồ Chí Minh
       const lat = 10.780221592300679;
       const log = 106.68759855218681 ;
+      
       // khởi tạo bản đồ 
       const map = L.map('map').setView([lat, log] , 17);
+     
       // nền bản đồ openStreetMap
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png' , {
         maxZoom : 20,
@@ -17,3 +18,5 @@
       const marker = L.marker([lat , log] ).addTo(map);
       // thông tin maker 
       marker.bindPopup('<b>273 Điện Biên Phủ , Phường Xuân Hòa , <br> Thành Phố Hồ chí Minh.</b>');
+      
+      
